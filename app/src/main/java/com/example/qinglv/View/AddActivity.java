71612e03a6eb118328afeb419d4f53d7b4152e39
xu qi <xchos7@163.com>
2019-07-22 21:38:14 +0800

    添加了拍照布局

---
+++ app/src/main/java/com/example/qinglv/View/AddActivity.java
@@ -0,0 +1,15 @@
+package com.example.qinglv.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.qinglv.R;
+
+public class ShareActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+    }
+}
