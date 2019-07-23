19654ca0d00c006aa70f08777b50ada672a835b0
huan <172802065@qq.com>
2019-07-23 12:11:15 +0800

    <增加活动>增加了一个个人文章界面的活动（尚未完成）

---
+++ app/src/main/java/com/example/qinglv/MainPackage/View/ArticleActivity.java
@@ -0,0 +1,19 @@
+package com.example.qinglv.MainPackage.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.qinglv.R;
+import com.sackcentury.shinebuttonlib.ShineButton;
+
+public class ArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article);
+
+        ShineButton shineButton = findViewById(R.id.shineButton_article_good);
+        shineButton.init(this);
+    }
+}
