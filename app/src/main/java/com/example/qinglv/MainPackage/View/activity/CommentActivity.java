16fb181950e098fceab687c500aa78bbbf504120
huan <172802065@qq.com>
2019-08-11 02:33:51 +0800

    <修改布局>修改了评论界面的布局

--- app/src/main/java/com/example/qinglv/MainPackage/View/activity/CommentActivity.java
+++ app/src/main/java/com/example/qinglv/MainPackage/View/activity/CommentActivity.java
@@ -1,14 +1,35 @@
 package com.example.qinglv.MainPackage.View.activity;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
 import com.example.qinglv.R;
+
+import java.util.Objects;
 
 public class CommentActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comment);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_comment);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setHomeButtonEnabled(true);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    //顶部返回按钮的监听
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
     }
 }
